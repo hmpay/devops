466234ebc9eafa16a69121174a24d3f256c74f94
郝丹辉 <haodanhui@laiyifen.com>
2020-11-08 10:18:35 +0800

    修改mysql创建标语句

--- src/hmpay/production/mysql/schema.sql
+++ src/hmpay/production/mysql/schema.sql
@@ -1,13 +1,11 @@
 -- 创建数据库
-create database `hmpay` default character set utf8 collate utf8_general_ci;
+create database if not exists `hmpay` default character set utf8 collate utf8_general_ci;
 
 use hmpay;
 
 -- 建表
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `created_at` bigint(40) DEFAULT NULL,
   `last_modified` bigint(40) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -18,6 +16,6 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- 插入数据
-INSERT INTO `user` (`id`, `created_at`, `last_modified`, `email`, `first_name`, `last_name`, `username`)
+INSERT INTO `user` (`created_at`, `last_modified`, `email`, `first_name`, `last_name`, `username`)
 VALUES
-    (0,1490257904,1490257904,'john.doe@example.com','John','Doe','user');
+    (1490257904,1490257904,'john.doe@example.com','John','Doe','user');
